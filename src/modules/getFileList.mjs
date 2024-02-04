ba5a81ec7e0f54e7db41ed2749645042a8c54e16
Greys73 <v.myazin@gmail.com>
2024-02-04 13:50:02 +0400

    feat: implement sorting in getFileList module Sun, Feb  4, 2024  1:50:01 PM

--- src/modules/getFileList.mjs
+++ src/modules/getFileList.mjs
@@ -6,6 +6,11 @@ const getFileList = async (path) => {
     const data = files.map((file) => {
       return { Name: file.name, Type: file.isDirectory() ? 'folder' : 'file' }
     });
+    data.sort((a, b) => {
+      if (a.Type.length < b.Type.length) return 1;
+      if (a.Type.length > b.Type.length) return -1;
+      return 0;
+    });
     return {data, error: null};
   } catch(error) {
     return {data: null, error};
